331e467b08cd8deff12a9d1a48a030453527ae51
chosenonehacks <3025925+chosenonehacks@users.noreply.github.com>
2023-02-20 10:01:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        <!-- Atos Red Team subdomain takenover! -->
+          Get started by editing <code>pages/index.js</code>        
         </p>
       </main>
 
