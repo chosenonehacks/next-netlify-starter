0abb07b95c283d1f1d5eb174016ca786729ae05b
chosenonehacks <3025925+chosenonehacks@users.noreply.github.com>
2023-02-20 10:06:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>        
+          Get started by editing <code>pages/index.js</code>
+          <!-- https://sportsclub-pp.paris2024.org -->
         </p>
       </main>
-
       <Footer />
     </div>
   )
